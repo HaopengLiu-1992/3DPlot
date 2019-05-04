4becc5ceda54589be7a71133b3936cd0e8dc598e
Haopeng Liu <haliu@zendesk.com>
2019-05-04 01:18:24 -0700

    [3DPlot] remove useless lines

--- run_script.py
+++ run_script.py
@@ -1,10 +1,6 @@
 from data_import import DataImport
 from build_coordinates import BuildCoordinates
 
-
-
-# For each set of style and range settings, plot n random points in the box
-# defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
 a = DataImport("data/centerline.csv")
 b = BuildCoordinates(a.get_xs(), a.get_ys(), a.get_zs())
 b.plot('red', 'o')
